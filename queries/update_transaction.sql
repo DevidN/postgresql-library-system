a12ce2c9df81c985dc19b2280ea0bda851c4cae8
DevidN <94879271+DevidN@users.noreply.github.com>
2026-04-14 13:53:59 +0300

    Create update_transaction.sql

---
+++ queries/update_transaction.sql
@@ -0,0 +1,25 @@
+# Задача: Библиотекарь выдал книгу 'Война и мир' читателю. Нужно изменить статус этой книги на true.
+UPDATE books
+SET is_borrowed = TRUE
+WHERE books.title = 'Война и мир'
+RETURNING*;
+# Задача: Библиотекарь начал оформлять выдачу книги «Преступление и наказание», но в последний момент читатель передумал. Изменение нужно отменить, не трогая остальные данные.
+BEGIN;
+
+UPDATE books
+SET is_borrowed = TRUE
+WHERE books.title = 'Преступление и наказание'
+RETURNING is_borrowed;
+
+ROLLBACK;
+
+# Задача: Библиотекарь выдал книгу «Братья Карамазовы» успешно
+
+BEGIN;
+
+UPDATE books
+SET is_borrowed = TRUE
+WHERE books.title = 'Преступление и наказание'
+RETURNING is_borrowed;
+
+COMMIT;
