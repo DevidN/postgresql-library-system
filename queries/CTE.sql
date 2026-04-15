1ac525438f820a7cf6c3f5a717f96724708060cd
DevidN <94879271+DevidN@users.noreply.github.com>
2026-04-15 11:36:19 +0300

    Update CTE.sql

--- queries/CTE.sql
+++ queries/CTE.sql
@@ -33,3 +33,16 @@ WHERE book_count > (SELECT AVG (book_count) FROM book_counts)
 SELECT a.name
 FROM authors a
 WHERE a.id IN (SELECT author_id FROM avg_count);
+
+
+-- Найти всех авторов, у которых хотя бы одна книга выдана. Вывести имена авторов без дубликатов.
+WITH borrowed_books AS(
+SELECT author_id, is_borrowed
+FROM books
+WHERE is_borrowed = TRUE
+)
+
+SELECT DISTINCT a.name
+FROM authors a
+INNER JOIN borrowed_books ON a.id = borrowed_books.author_id;
+
