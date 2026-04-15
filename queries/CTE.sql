6b20e2a3428a848e02caf97377347d1aa369290a
DevidN <94879271+DevidN@users.noreply.github.com>
2026-04-15 09:30:53 +0300

    Create CTE.sql

---
+++ queries/CTE.sql
@@ -0,0 +1,15 @@
+#Найти всех авторов, у которых больше одной книги, а потом вывести все их книги (название, год).
+
+#Создал временную таблицу, сделал выборку авторов, у которых больше 1 книги
+WITH rich_authors AS(
+SELECT author_id
+FROM books
+GROUP BY author_id
+HAVING COUNT(*) > 1
+)
+SELECT a.name, b.title, b.published_year
+FROM books b
+INNER JOIN authors a ON b.author_id = a.id
+#Использовал эту конструкция, т.к. она лучше для чтения + если бы были дубликаты, inner join умножил бы их 
+WHERE b.author_id IN (SELECT author_id FROM rich_authors)
+ORDER BY a.name, b.published_year;
