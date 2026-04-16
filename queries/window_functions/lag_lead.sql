622c1ef1df65d299259193b8cbd10a6dc168a957
DevidN <94879271+DevidN@users.noreply.github.com>
2026-04-16 15:57:34 +0300

    Update lag_lead.sql

--- queries/window_functions/lag_lead.sql
+++ queries/window_functions/lag_lead.sql
@@ -1,1 +1,12 @@
-
+#Вывести для книг только Толстого:название книги, год издания, год предыдущего издания этой же книги, год следующего издания этой же книги
+SELECT 
+b.title,
+e.published_year,
+LAG(e.published_year) OVER (PARTITION BY b.id ORDER BY e.published_year) AS prev_year,
+LEAD(e.published_year) OVER (PARTITION BY b.id ORDER BY e.published_year) AS next_year
+FROM editions e
+JOIN books b ON e.book_id = b.id
+JOIN book_authors ba ON b.id = ba.book_id
+JOIN authors a ON ba.author_id = a.id
+WHERE a.name = 'Лев Толстой'
+ORDER BY b.title, e.published_year;
