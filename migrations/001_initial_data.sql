750903e19a46d2b6ab4a426d4f5080fe4e40f00f
DevidN <94879271+DevidN@users.noreply.github.com>
2026-04-16 12:05:35 +0300

    Update 001_initial_data.sql

--- migrations/001_initial_data.sql
+++ migrations/001_initial_data.sql
@@ -1,4 +1,4 @@
-#Вставляем данные в book_authors и editions из books, затем перейменовываем таблицы в books_old
+#Вставляем данные в book_authors и editions из books, затем перейменовываем таблицы в books_old. Затем вставляем в новую таблицу books названия книг из books_old
 INSERT INTO book_authors (book_id, author_id)
 SELECT id, author_id FROM books;
 
@@ -6,3 +6,5 @@ INSERT INTO editions (book_id, published_year, is_borrowed)
 SELECT id, published_year, is_borrowed FROM books;
 
 ALTER TABLE books RENAME TO books_old;
+
+INSERT INTO books (title) SELECT DISTINCT title FROM books_old;
