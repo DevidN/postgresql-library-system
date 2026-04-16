9efd46dc5404c5f26c60669d431bca84123d22f9
DevidN <94879271+DevidN@users.noreply.github.com>
2026-04-16 11:20:28 +0300

    Update normalize_books.sql

--- schema/normalize_books.sql
+++ schema/normalize_books.sql
@@ -23,3 +23,31 @@ INSERT INTO editions (book_id, published_year, is_borrowed)
 SELECT id, published_year, is_borrowed FROM books;
 
 ALTER TABLE books RENAME TO books_old;
+
+-- Создаем таблицу books и делаем миграцию данных в предыдущие таблицы т.к. значения из них ссылаются на books, но она теперь books_old
+
+CREATE TABLE books(
+id SERIAL PRIMARY KEY,
+title TEXT NOT NULL
+);
+
+INSERT INTO books (title) SELECT DISTINCT title FROM books_old;
+
+ALTER TABLE book_authors ADD COLUMN new_book_id INTEGER;
+
+# Производим миграцию данных через создание новой колонки в таблице
+UPDATE book_authors as ba
+SET new_book_id = b.id
+FROM books_old AS bo
+JOIN books AS b ON bo.title = b.title
+WHERE ba.book_id = bo.id;
+
+# удаляем book_id, меняем new_book_id на book_id и возвращаем PKKEY и NOTNULL ограничение
+ALTER TABLE book_authors DROP COLUMN book_id;
+ALTER TABLE book_authors RENAME COLUMN new_book_id TO book_id;
+ALTER TABLE book_authors ALTER COLUMN book_id SET NOT NULL;
+ALTER TABLE book_authors ADD CONSTRAINT fk_book_authors_book FOREIGN KEY (book_id) REFERENCES books(id);
+ALTER TABLE book_authors ADD PRIMARY KEY (book_id, author_id);
+
+#Делаем аналогичную миграцию данных для таблицы 
+
