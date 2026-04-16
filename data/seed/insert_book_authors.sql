3c61bf9c11c4c602332ad896ef390faf32d1745d
DevidN <94879271+DevidN@users.noreply.github.com>
2026-04-16 13:27:49 +0300

    Update insert_book_authors.sql

--- data/seed/insert_book_authors.sql
+++ data/seed/insert_book_authors.sql
@@ -1,7 +1,9 @@
-INSERT INTO book_authors (author_id, book_id) VALUES (1, 1),
-  (1, 1),
-  (1, 4),
-  (1, 6),
-  (2, 2),
-  (2, 5),
-  (3, 3);
+INSERT INTO book_authors (book_id, author_id)
+SELECT b.id, a.id
+FROM books b, authors a
+WHERE (b.title = 'Война и мир' AND a.name = 'Лев Толстой')
+   OR (b.title = 'Анна Каренина' AND a.name = 'Лев Толстой')
+   OR (b.title = 'Письма' AND a.name = 'Лев Толстой')
+   OR (b.title = 'Преступление и наказание' AND a.name = 'Фёдор Достоевский')
+   OR (b.title = 'Братья Карамазовы' AND a.name = 'Фёдор Достоевский')
+   OR (b.title = '1984' AND a.name = 'Джордж Оруэлл');
