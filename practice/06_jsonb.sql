b529cea579b702d062ea06252c5a5552255dac83
DevidN <94879271+DevidN@users.noreply.github.com>
2026-04-21 14:48:14 +0300

    Update 06_jsonb.sql

--- practice/06_jsonb.sql
+++ practice/06_jsonb.sql
@@ -1,3 +1,5 @@
+# Научиться хранить, индексировать и искать JSON-данные.JSON — текстовое хранение (медленнее, без проверки), JSONB — бинарное хранение (быстрее, сжато, индексируется)
+
 DROP TABLE IF EXISTS events;
 CREATE TABLE events (
     id SERIAL PRIMARY KEY,
@@ -12,37 +14,37 @@ INSERT INTO events (event_data) VALUES
     ('{"user_id": 3, "action": "view", "page": "/catalog"}'),
     ('{"user_id": 2, "action": "login", "ip": "192.168.1.10"}');
 
-#1
+#1 Извлечение полей
 SELECT 
 event_data->>'user_id' AS user_id,
 event_data->> 'action' AS action
 FROM events;
 
-#2
+#2 Фильтрация по action
 SELECT *
 FROM events
 WHERE event_data @> '{"action": "login"}';
 
-#3
+#3 Поиск по наличию ключа
 SELECT *
 FROM events
 WHERE event_data ? 'items';
 
-#4
+#4 Поиск значения в массиве
 SELECT event_data->>'items' AS items
 FROM events
 WHERE event_data @> '{"items": ["book"]}';
 
-#5
+#5 Создание индекса
 CREATE INDEX idx_events_jsonb ON events USING GIN (event_data);
 EXPLAIN ANALYZE SELECT * FROM events WHERE event_data @> '{"action": "login"}';
 
-#6
+#6 Добавление поля
 UPDATE events
 SET event_data = event_data || '{"session_id ": "abc123"}'
 WHERE events.id = 1;
 
-#7
+#7 Удаление поля
 UPDATE events 
 SET event_data = event_data - 'session_id'
 WHERE id = 1;
